6439c325497e851775c88214a52b531ed7b9efc7
ammaradel17 <amar170518@feng.bu.edu.eg>
2022-10-01 00:55:05 +0200

    fixing bugs

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://www.ticketing-app-demo.me/',
+        'http://www.ticketing-app-demo.me',
       headers: req.headers,
     });
   } else {
