0bca151fefea469c4b67de8019415e2861319b0c
ammaradel17 <amar170518@feng.bu.edu.eg>
2022-10-01 02:03:21 +0200

    fixing bugs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,19 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false, // Don't build indexes
+    maxPoolSize: 10, // Maintain up to 10 socket connections
+     // @ts-ignore
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    family: 4 // Use IPv4, skip trying IPv6
+  }
   try {
-    await mongoose.connect(process.env.MONGO_URI!, {});
+    await mongoose.connect(process.env.MONGO_URI!,options);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
